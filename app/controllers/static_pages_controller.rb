9a29ba6774ee9a4a56433fd922093d8296fe6268
ismaelsanchez73 <sanchezismaelnoe@gmail.com>
2024-10-03 22:52:36 -0600

    Subiendo el proyecto de la guia 8

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,14 @@
+class StaticPagesController < ApplicationController
+  def home
+    # Puedes agregar código aquí si es necesario
+  end
+
+  def help
+    # Puedes agregar código aquí si es necesario
+  end
+
+  def about
+    
+  end
+end
+
